04e24247d37b41feb7a1b2d31feff3f03c36a89f
aadi-14 <adisharma14500@gmail.com>
2024-03-25 17:57:14 +0530

    Added Second Example to it
    The second example is to show how we can directly call parentfunction

--- closure.js
+++ closure.js
@@ -3,7 +3,7 @@
 **/
 
 // Example 1: Parent and Child Function(s)
-let a = 1;
+/* let a = 1;
 const parentFunction = () => {
     let b = 2;
     console.log(a);
@@ -18,3 +18,19 @@ const result = parentFunction(); // When we are calling parentFunction it is onl
 
 result();   // But when we are calling its child function then it is able to utilize all the variables global and parentFunction has
 result();   // Multiple time calling this same function will update the initial value
+ */
+
+// Example 2: Directily initializing the function 
+let c = 3;
+const initialOne = (() => {
+    let d = 4;
+    console.log(c);
+    console.log(d);
+    return () => {
+        console.log(c += 3);
+        console.log(d += 4);
+    }
+})();       // It is the same thing But we did't store parentFunction in any variable instead we directly called it
+//  Here we also have the option to pass any default parameters in ParentFunction
+
+initialOne();   // And Similerly we can call the child function
