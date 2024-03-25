d27c36d87fa2ed208402386711834f3273292381
aadi-14 <adisharma14500@gmail.com>
2024-03-25 16:52:37 +0530

    Newly learned Closure concept

---
+++ closure.js
@@ -0,0 +1,20 @@
+/**
+ * Closure Example 
+ * **/
+
+let a = 1;
+
+const parentFunction = () => {
+    let b = 2;
+    console.log(a);
+    console.log(b);
+
+    return () => {
+        console.log(a += 3);
+        console.log(b += 4);
+    }
+}
+const result = parentFunction(); // When we are calling parentFunction it is only executing the action defined in it 
+
+result();   // But when we are calling its child function then it is able to utilize all the variables global and parentFunction has
+result();   // Multiple time calling this same function will update the initial value
